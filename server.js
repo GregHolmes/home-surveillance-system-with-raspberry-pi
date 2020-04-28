56a6f280e5581a8422126b3ddb1507cabc99a243
Greg Holmes <greg.holmes2@vonage.com>
2020-04-28 14:57:00 +0100

    Added nexmo as beta package, and included new SMS

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ const app = express();
 const path = require('path');
 const https = require('https');
 const fs = require('fs');
-require('dotenv').config();
+const dotenv = require('dotenv');
 const gpio = require('onoff').Gpio;
 const pir = new gpio(18, 'in', 'both');
 const ngrok = require('ngrok');
+
+dotenv.config();
 
 const opentok = new OpenTok(
     process.env.OPENTOK_API_KEY,
@@ -22,17 +24,18 @@ const nexmo = new Nexmo({
     apiSecret: process.env.NEXMO_API_SECRET
   })
 
-let canCreateSession = true;
+var canCreateSession = true;
+
 // Triggers the whole process of creating a session, adding the the session id to the database.
 // Opens a headless mode for the publisher view.
 // Will send a text message.
 startServer();
 
 pir.watch(function(err, value) {
-    console.log('oh?');
     if (value == 1 && canCreateSession == true) {
         canCreateSession = false;
         console.log('Motion has been detected');
+
         createSession();
     }
 });
@@ -45,7 +48,7 @@ async function createSession() {
           return null;
         } else {
           createSessionEntry(session.sessionId);
-          // sendSMS();
+          sendSMS();
           startPublish();
         }
       });
@@ -58,37 +61,34 @@ async function createSessionEntry(sessionId) {
 }
 
 async function connectNgrok() {
-    console.log('testing');
   const url = await ngrok.connect({
-    proto: 'http', // http|tcp|tls, defaults to http
-    addr: 'https://localhost:3000', // port or network address, defaults to 80
-    subdomain: 'gregdev', // reserved tunnel name https://alex.ngrok.io
-    region: 'eu', // one of ngrok regions (us, eu, au, ap), defaults to us
-    configPath: '/home/pi/.ngrok2/ngrok.yml', // custom path for ngrok config file
-    onStatusChange: status => { console.log(status)}, // 'closed' - connection is lost, 'connected' - reconnected
+    proto: 'http',
+    addr: 'https://localhost:3000', 
+    subdomain: 'gregdev',
+    region: 'eu',
+    configPath: '/home/pi/.ngrok2/ngrok.yml',
+    onStatusChange: status => { console.log(status)},
     onLogEvent: data => { console.log(data) }
   });
-    console.log(url);
-    console.log('endtesting');
 }
 
-// function sendSMS() {
-//     const from = process.env.NEXMO_BRAND_NAME;
-//     const to = process.env.TO_NUMBER;
-//     const text = 'Motion has been detected on your camera, please view the link here:';
+function sendSMS() {
+    const message = {
+      content: {
+        type: 'text',
+        text: 'Motion has been detected on your camera, please view the link here: ' + process.env.DOMAIN,
+      },
+    };
+    
+    nexmo.channel.send(
+      { type: 'sms', number: process.env.TO_NUMBER },
+      { type: 'sms', number: process.env.NEXMO_BRAND_NAME },
+      message,
+      (err, data) => { console.log(data.message_uuid); },
+      { useBasicAuth: true },
+    );
+}
 
-//     nexmo.message.sendSms(from, to, text, (err, responseData) => {
-//         if (err) {
-//             console.log(err);
-//         } else {
-//             if(responseData.messages[0]['status'] === "0") {
-//                 console.log("Message sent successfully.");
-//             } else {
-//                 console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`);
-//             }
-//         }
-//     });
-// }
 async function startServer() {
   const port = 3000;
 
@@ -121,12 +121,8 @@ async function startServer() {
     passphrase: 'testpass'
   }, app);
 
-  httpServer.listen(port, (err) => {
-    if (err) return console.log(`Something bad happened: ${err}`);
-    console.log(`Node.js server listening on ${port}`);
-
+  httpServer.listen(() => {
     connectNgrok();
-
   });
 }
 
@@ -153,7 +149,7 @@ async function startPublish() {
   await page.goto('https://localhost:3000/serve');
 
   async function closeSession(page, browser) {
-    console.log('delay expired');
+    console.log('Time limit expired. Closing stream');
     await page.close();
     await browser.close();
   }
